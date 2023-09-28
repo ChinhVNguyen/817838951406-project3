f1f64d4cc0d3740b6f204beac457e4401b223fc3
ChinhVNguyen <chinhfptusa@gmail.com>
2023-09-28 14:11:52 +0700

    Change localhost to reverseproxy, add screenshot

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4f92278cdda949e6a1ba76c01ed57a6-1752376011.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
